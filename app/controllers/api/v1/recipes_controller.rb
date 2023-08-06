085be9578aecf8dc52bc7bf8ca145653b35495b1
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-06 00:34:59 -0600

    remove pry

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    require 'pry'; binding.pry
     render json: RecipeSerializer.new(RecipeFacade.new(params).recipes_by_country)
   end
 end
