433618847fb265d06c10f07e86ff8e5ac0f8a7d4
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-06 15:21:04 -0600

    add image poro and test

---
+++ spec/poros/image_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper" 
+
+RSpec.describe Image do 
+  describe "initialize" do 
+    it "can intialize with attributes" do 
+      image = Image.new("awesome_url", "description of image")
+
+      expect(image.url).to eq("awesome_url")
+      expect(image.alt_tag).to eq("description of image")
+    end
+  end
+end
