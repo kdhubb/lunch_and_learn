5f370042f7b6097256e7d103f80a85e9dabd4093
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-06 16:13:14 -0600

    add count to images test

--- spec/requests/api/v1/learning_resources/index_spec.rb
+++ spec/requests/api/v1/learning_resources/index_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "Learning Resource", type: :request do
       expect(learning_resource[:attributes][:video][:title]).to eq("A Super Quick History of Laos")
       expect(learning_resource[:attributes][:video][:youtube_video_id]).to eq("uw8hjVqxMXw")
       expect(learning_resource[:attributes][:images]).to be_an(Array)
+      expect(learning_resource[:attributes][:images].count).to eq(10)
       expect(learning_resource[:attributes][:images][0][:url]).to eq("https://images.unsplash.com/photo-1540611025311-01df3cef54b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODU0Njl8MHwxfHNlYXJjaHwxfHxsYW9zfGVufDB8fHx8MTY5MTM1OTY3Nnww&ixlib=rb-4.0.3&q=80&w=1080")
       expect(learning_resource[:attributes][:images][0][:alt_tag]).to eq("time lapse photography of flying hot air balloon")
     end
