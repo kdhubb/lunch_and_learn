6d6ee2bce5503015acc269f83f70f239a8360a4b
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-05 20:37:31 -0600

    add recipe service

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,18 @@
+class RecipeService
+  def conn
+    Faraday.new(url: "https://api.edamam.com/api/recipes/v2") do |faraday|
+      faraday.params['app_id'] = ENV['EDAMAM-ID']
+      faraday.params['app_key'] = ENV["EDAMAM-KEY"]
+      faraday.params["type"] = "public"
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_recipes_by_country(country)
+    stuff = get_url("?q=#{country}")
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do 
+    namespace :v1 do 
+      resources :recipes, only: [:index]
+      resources :favorites, only: [:post]
+      resources :learning_resources, only: [:index]
+    end
+  end
+end
