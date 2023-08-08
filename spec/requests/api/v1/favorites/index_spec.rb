90fa38cd35b9bba9dbf7b47ce6f802b4cd377a02
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-07 19:22:26 -0600

    add no favorites test

--- spec/requests/api/v1/favorites/index_spec.rb
+++ spec/requests/api/v1/favorites/index_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe "Get Favorites" do 
   describe "get a user's favorites" do 
     before(:each) do 
-      @user = User.create!(name: 'Wolfie', email: 'wolfiebarks@wolfie.com', password: 'password', api_key: 'api_key5')
-      @fav1 = @user.favorites.create!(country: "Thailand", recipe_link: "drunken_noodle_url", recipe_title: "drunken noodles")
-      @fav2 = @user.favorites.create!(country: "Mexico", recipe_link: "chiles_enogadas_url", recipe_title: "chiles enogadas")
+      @user1 = User.create!(name: 'Wolfie', email: 'wolfiebarks@wolfie.com', password: 'password', api_key: 'api_key5')
+      @user2 = User.create!(name: 'Wolfie', email: 'wolfiechews@wolfie.com', password: 'password', api_key: 'api_key3')
+      @fav1 = @user1.favorites.create!(country: "Thailand", recipe_link: "drunken_noodle_url", recipe_title: "drunken noodles")
+      @fav2 = @user1.favorites.create!(country: "Mexico", recipe_link: "chiles_enogadas_url", recipe_title: "chiles enogadas")
     end
     it "sends all user's favories happy path" do 
       get "/api/v1/favorites?api_key=api_key5"
@@ -22,7 +23,17 @@ RSpec.describe "Get Favorites" do
       expect(parsed[:data][0][:attributes][:recipe_link]).to eq("drunken_noodle_url")
       expect(parsed[:data][0][:attributes][:country]).to eq("Thailand")
       expect(parsed[:data][0][:attributes][:created_at]).to be_a(String)
-      
+    end
+    it "sends all user's favories happy path no favorites" do 
+      get "/api/v1/favorites?api_key=api_key3"
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expected = {
+          "data": []
+      }
+      expect(parsed[:data]).to be_an(Array)
+      expect(parsed).to eq(expected)
     end
   end
 end
