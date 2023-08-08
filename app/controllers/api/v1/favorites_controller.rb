7411de057afad0d77150cd787f1f2e49f5e601fe
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-07 18:19:49 -0600

    add favorites and model associations

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::FavoritesController < ApplicationController
+  def create
+    
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true 
   validates :name, presence: true
   validates :password, presence: true
+  has_many :favorites
 
   has_secure_password
 end
