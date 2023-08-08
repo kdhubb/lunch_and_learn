ebacc7d56a71bf8ba2b18a08ef4b9d4a7ac346fb
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-07 18:47:06 -0600

    add sad path test

--- spec/requests/api/v1/favorites/create_spec.rb
+++ spec/requests/api/v1/favorites/create_spec.rb
@@ -24,4 +24,32 @@ RSpec.describe "Create Favorites", type: :request do
       expect(Favorite.last.recipe_title).to eq("Crab Fried Rice (Khaao Pad Bpu)")
     end
   end
+  describe "create a favorite sad paths" do 
+    it "returns an error for bad api key" do 
+      user = User.create!(name: "Wolfie", email: "wolfiebarks@wolfie.com", password: "password", api_key: "api_key")
+      params = {
+        "api_key": "api_key5",
+        "country": "thailand",
+        "recipe_link": "https://www.tastingtable.com/.....",
+        "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post "/api/v1/favorites", headers: headers, params: JSON.generate(params)
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expected = {
+          errors: [
+              {
+                  status: "404",
+                  title: "Invalid API key"
+              }
+          ]
+      }
+
+      expect(response.code).to eq("404")
+      expect(parsed).to eq(expected)
+      expect(Favorite.last).to be(nil)
+    end
+  end
 end
